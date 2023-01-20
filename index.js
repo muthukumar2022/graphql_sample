abaad2387398b914b58bef1611371be3ccbb6a61
muthukumar2022 <kumarmuthu.tech@gmail.com>
2023-01-20 15:15:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const {ApolloServer}=require('apollo-server-express')
+const mongoose=require('mongoose');
+const typeDefs = require('./typeDefs');
+const resolvers =require ('./resolvers');
+
+
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://localhost:27017/graphqltable",{ useNewUrlParser: true})
+.then((db)=>{console.log("DB CONNECT")})
+.catch((err)=>{console.log(err)})
+
+
+const startServer = async()=>{
+    const app=express();
+    const apolloServer=new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app:app});
+    app.listen(4000,()=>{console.log("Server running *4000")})
+
+}
+startServer();
